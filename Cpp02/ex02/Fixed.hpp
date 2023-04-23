4465fe30a582902d328aecc4185529d6f1fa6efe
MKM327 <85278762+MKM327@users.noreply.github.com>
2023-04-23 19:05:16 +0300

    ex02

---
+++ Cpp02/ex02/Fixed.hpp
@@ -0,0 +1,64 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <iostream>
+
+class Fixed {
+private:
+	int fixed_point;
+	static const int bits = 8;
+public:
+	Fixed();
+
+	Fixed(const Fixed &copy);
+
+	~Fixed();
+
+	Fixed(int number);
+
+	Fixed(const float floatNum);
+
+	int getRawBits();
+
+	float toFloat(void) const;
+
+	int toInt(void) const;
+
+	void setFixedPoint(int fixedPoint);
+
+	Fixed &operator=(const Fixed &copy);
+
+	bool operator>(const Fixed &right) const;
+
+	bool operator<(const Fixed &right) const;
+
+	bool operator<=(const Fixed &right) const;
+
+	bool operator>=(const Fixed &right) const;
+
+	bool operator==(const Fixed &right) const;
+
+	bool operator!=(const Fixed &right) const;
+
+	Fixed &operator++();
+
+	Fixed operator++(int);
+
+	Fixed &operator--();
+
+	Fixed operator--(int);
+
+	float operator+(Fixed fixed);
+	float operator-(Fixed &fixed);
+	float operator/(Fixed &fixed);
+	float operator*(Fixed fixed) const;
+	static Fixed & max(Fixed &fixedNum1, Fixed &fixedNum2);
+	static const Fixed & max(const Fixed &fixedNum1,const Fixed &fixedNum2);
+	static Fixed &min(Fixed &fixedNum1, Fixed &fixedNum2);
+	static const Fixed &min(const Fixed &fixedNum1,const Fixed &fixedNum2);
+};
+
+// define outside because the second parameter automatically becomes this
+std::ostream	&operator<<(std::ostream &o, Fixed const &fixed);
+
+
+#endif
