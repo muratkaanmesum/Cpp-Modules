e75b67b0a7f6b28c7d57c06f264279ce8fa68ec8
mmesum <kaanmesum@gmail.com>
2023-08-16 12:43:14 +0300

    06 fixes

--- Cpp06/ex00/ConverterUtils.cpp
+++ Cpp06/ex00/ConverterUtils.cpp
@@ -1,11 +1,11 @@
 #include "ScalarConverter.hpp"
-void convertToChar(ScalarConverter *sc )
+void convertToChar(ScalarConverter *sc)
 {
     std::cout << "char: ";
-    int num = std::strtol(sc->getStr().c_str(),NULL,10);
-    if(num < 0 || num > 127 || std::isnan(num))
+    int num = std::strtol(sc->getStr().c_str(), NULL, 10);
+    if (num < 0 || num > 127 || std::isnan(num))
         std::cout << "impossible" << std::endl;
-    else if((num > 0 && num < 32) || num == 127)
+    else if ((num > 0 && num < 32) || num == 127)
         std::cout << "non printable" << std::endl;
     else
         std::cout << static_cast<char>(num) << std::endl;
@@ -22,26 +22,26 @@ void printInt(ScalarConverter *sc)
 {
     convertToChar(sc);
     std::cout << "int: " << sc->getStr() << std::endl;
-    int num = std::strtol(sc->getStr().c_str(),NULL,10);
+    int num = std::strtol(sc->getStr().c_str(), NULL, 10);
     std::cout << "float: " << static_cast<float>(num) << ".0f" << std::endl;
     std::cout << "double: " << static_cast<double>(num) << ".0" << std::endl;
 }
 void printFloat(ScalarConverter *sc)
 {
-    size_t decimalPlaces = sc->getStr().size() - sc->getStr().find('.') - 1;
-    std::cout << "char: impossible" << std::endl;
-    float num = std::strtof(sc->getStr().c_str(),NULL);
+    size_t decimalPlaces = sc->getStr().size() - sc->getStr().find('.') - 2;
+    float num = std::strtof(sc->getStr().c_str(), NULL);
+    convertToChar(sc);
     std::cout << "int: " << static_cast<int>(num) << std::endl;
-    std::cout << std::fixed << std::setprecision((int)decimalPlaces) << "float: "<< num << "f" << std::endl;
+    std::cout << std::fixed << std::setprecision((int)decimalPlaces) << "float: " << num << "f" << std::endl;
     std::cout << "double: " << static_cast<double>(num) << std::endl;
 }
 void printDouble(ScalarConverter *sc)
 {
     size_t decimalPlaces = sc->getStr().size() - sc->getStr().find('.') - 1;
-    double num = std::strtod(sc->getStr().c_str(),NULL);
-    std::cout << "char: impossible" << std::endl;
+    double num = std::strtod(sc->getStr().c_str(), NULL);
+    convertToChar(sc);
     std::cout << "int: " << static_cast<int>(num) << std::endl;
-    std::cout << std::fixed << std::setprecision((int)decimalPlaces) << "float: "<< static_cast<float>(num) << "f" << std::endl;
+    std::cout << std::fixed << std::setprecision((int)decimalPlaces) << "float: " << static_cast<float>(num) << "f" << std::endl;
     std::cout << "double: " << num << std::endl;
 }
 void printNan()
@@ -49,19 +49,19 @@ void printNan()
     std::cout << "char: impossible" << std::endl;
     std::cout << "int: impossible" << std::endl;
     std::cout << "float: nanf" << std::endl;
-    std::cout << "double: nan" <<  std::endl;
+    std::cout << "double: nan" << std::endl;
 }
 void printInf()
 {
     std::cout << "char: impossible" << std::endl;
     std::cout << "int: impossible" << std::endl;
     std::cout << "float: inff" << std::endl;
-    std::cout << "double: inf" <<  std::endl;
+    std::cout << "double: inf" << std::endl;
 }
 void printNegInf()
 {
     std::cout << "char: impossible" << std::endl;
     std::cout << "int: impossible" << std::endl;
     std::cout << "float: -inff" << std::endl;
-    std::cout << "double: -inf" <<  std::endl;
+    std::cout << "double: -inf" << std::endl;
 }
