71af5bfc57a04ca72d9b5ce74caf5550d8449c91
MKM327 <85278762+MKM327@users.noreply.github.com>
2023-08-18 14:32:19 +0300

    nearly finished ex00

--- Cpp09/ex00/BitcoinExchange.hpp
+++ Cpp09/ex00/BitcoinExchange.hpp
@@ -8,13 +8,20 @@
 #include <utility>
 #include <vector>
 #include <list>
+#include <cstdlib>
+#define BAD_INPUT "Error: Bad input => "
+#define TOO_LARGE "Error: Too large of a number"
+#define NOT_POSITIVE "Error: Not a positive number"
 class BitcoinExchange
 {
 private:
     std::list<std::pair<std::string, double> > db;
     std::list<std::pair<std::string, double> > inputFile;
     void readDbFile();
-
+    void readInputFile(std::ifstream &file);
+    void checkInputFile();
+    void checkDates();
+    void checkValues();
 public:
     BitcoinExchange();
     BitcoinExchange(std::ifstream &file);
